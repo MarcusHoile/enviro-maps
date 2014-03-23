ff27f9970f5eba5f008ecf672b1060dcf164e1ca
MollyMoile <marcus.hoile@gmail.com>
2014-03-24 09:43:56 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EnviroMaps::Application.config.secret_key_base = '9a99bd84acd97ab50dbcd25576a7b0cc0e9d70a6c78757fafdd53f9536801aa3aa49d250c4462869f7235145ac46af9294ba14caa7e4a624b981a6f92bd11f46'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EnviroMaps::Application.config.session_store :cookie_store, key: '_enviro-maps_session'
