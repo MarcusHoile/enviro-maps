fa4d86ea55ef9dacf6c7854b5b88f3dadcec439a
MollyMoile <marcus.hoile@gmail.com>
2014-03-24 14:44:30 +1100

    add collection of markers to the map

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -31,56 +31,38 @@ function initialize() {
 }
 
 function createMarker() {
-  var  marker = gon.marker
-  var myLatlng = new google.maps.LatLng(marker.lat, marker.lng);
+  var markers = gon.markers
+  var markerObj = gon.marker
+  var myLatlng = new google.maps.LatLng(markerObj.lat, markerObj.lng);
 
-  var marker = new google.maps.Marker({
-    position: myLatlng,
-    map: map,
-    title: 'Uluru (Ayers Rock)'
-  });
-
-
-
+  // place single marker on the map
+  // var marker = new google.maps.Marker({
+  //   position: myLatlng,
+  //   map: map,
+  // });
 
 
 
-
-
-
-
-
-  // var results = gon.markers;
-  // // marker = new google.maps.Marker({
-  // //   map: map,
-  // //   position: place.geometry.location
-  // // });
-  // // markers.push(marker);
-
-  // // on click of marker show custom content in infowindow
+  // on click of marker show custom content in infowindow
   // google.maps.event.addListener(marker, 'click', function() {
   //   infowindow.setContent(place.name + "<br><br><button>do something</button>");
   //   // infowindow.setContent();
   //   infowindow.open(map, this);
   // });
 
-  // // Create a marker on the map for each in markers 
-  // for (var i = 0; i < results.length; i++) {
-  //   var markerLetter = String.fromCharCode('A'.charCodeAt(0) + i);
-  //   var markerIcon = MARKER_PATH + markerLetter + '.png';
-  //   // Use marker animation to drop the icons incrementally on the map.
-  //   markers[i] = new google.maps.Marker({
-  //     position: new google.maps.LatLng(3.589, 97.34699),
-  //     animation: google.maps.Animation.DROP,
-  //     icon: markerIcon
-  //   });
-  //   // If the user clicks a hotel marker, show the details of that hotel
-  //   // in an info window.
-  //   // markers[i].placeResult = results[i];
-  //   // google.maps.event.addListener(markers[i], 'click', showInfoWindow);
-  //   setTimeout(dropMarker(i), i * 100);
-  //   // addResult(results[i], i);
-  // }
+  // Create a marker on the map for each in markers 
+  for (var i = 0; i < markers.length; i++) {
+    markers[i] = new google.maps.Marker({
+      position: new google.maps.LatLng(markers[i].lat,markers[i].lng),
+      map: map
+    });
+    // If the user clicks a hotel marker, show the details of that hotel
+    // in an info window.
+    // markers[i].placeResult = results[i];
+    // google.maps.event.addListener(markers[i], 'click', showInfoWindow);
+    // setTimeout(dropMarker(i), i * 100);
+    // addResult(results[i], i);
+  }
 }
 
 
