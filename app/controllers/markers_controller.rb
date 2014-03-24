4ff17f5de3a14542900e42d4cc7d178c8363d117
MollyMoile <marcus.hoile@gmail.com>
2014-03-24 15:14:31 +1100

    added controller for issues

--- app/controllers/markers_controller.rb
+++ app/controllers/markers_controller.rb
@@ -1,9 +1,3 @@
 class MarkersController < ApplicationController
-	before_filter :authenticate_user!, except: [:index]
-	def index
-		gon.markers = Marker.all
-	end
 
-	def create
-	end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,10 @@
 class PagesController < ApplicationController
+	before_filter :authenticate_user!, except: [:index]
+	
+	def index
+		gon.markers = Marker.all
+	end
+
+	def create
+	end
 end
