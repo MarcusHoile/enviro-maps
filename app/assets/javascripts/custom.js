9db21ccf0f0c00158254d3bdd7522b2bfa2fd764
MollyMoile <marcus.hoile@gmail.com>
2014-03-24 16:05:19 +1100

    cleaned up js code, added info-content template

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -23,29 +23,14 @@ function initialize() {
   infowindow = new google.maps.InfoWindow({
     content: document.getElementById('info-content')
   });
-  createMarkers();
+  fetchIssues();
 }
 
-function createMarkers() {
+function fetchIssues() {
   var issues = gon.issues
-
-
-  // on click of marker show custom content in infowindow
-  // google.maps.event.addListener(marker, 'click', function() {
-  //   infowindow.setContent(place.name + "<br><br><button>do something</button>");
-  //   infowindow.open(map, this);
-  // });
-
   // Create a marker on the map for each in markers 
   for (var i = 0; i < issues.length; i++) {
-    // marker[i] = new google.maps.Marker({
-    //   position: new google.maps.LatLng(issues[i].lat,issues[i].lng),
-    //   map: map
-      createMarker(issues[i]);
-    
-    // If the user clicks a marker, show the details of that marker
-    // in an info window.
-
+    createMarker(issues[i]);
   }
 }
 
@@ -55,37 +40,22 @@ function createMarker(issue) {
     map: map,
     position: location
   });
+  // store all the markers in an array
   markers.push(marker);
 
   google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(issue.title + "<br><br><button>do something</button>");
-    // infowindow.setContent();
+    // infowindow.setContent("<h1>" + issue.title + "</h1><br><br><button>do something</button>");
+    buildIWContent(issue);
     infowindow.open(map, this);
   });
 }
 
-// Get the details for the marker. Show the information in an info window,
-// anchored on the marker that the user selected.
-function showInfoWindow() {
-  var marker = this;
 
-  infowindow.open(map, marker);
-  
-  // places.getDetails({reference: marker.placeResult.reference},
-  //   function(place, status) {
-  //     if (status != google.maps.places.PlacesServiceStatus.OK) {
-  //       return;
-  //     }
-  //     infowindow.open(map, marker);
-  //     buildIWContent(marker);
-  //   });
-}
-
-function buildIWContent(marker) {
-  document.getElementById('iw-url').innerHTML = '<b><a href="' + marker.url +
-  '">' + marker.organisation + '</a></b>';
-  document.getElementById('iw-description').textContent = marker.description;
-  document.getElementById('iw-title').textContent = marker.title;
+function buildIWContent(issue) {
+  document.getElementById('iw-url').innerHTML = '<b><a href="' + issue.url +
+  '">' + issue.organisation + '</a></b>';
+  document.getElementById('iw-description').textContent = issue.description;
+  document.getElementById('iw-title').textContent = issue.title;
 }
 
 
