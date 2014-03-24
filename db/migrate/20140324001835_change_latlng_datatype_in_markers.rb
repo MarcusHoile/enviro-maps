e9fb16bbf97ba019fd012b1d319b9f8f2b05b284
MollyMoile <marcus.hoile@gmail.com>
2014-03-24 15:18:02 +1100

    deleted redundant migration

--- db/migrate/20140324001835_change_latlng_datatype_in_markers.rb
+++
@@ -1,7 +0,0 @@
-class ChangeLatlngDatatypeInMarkers < ActiveRecord::Migration
-  def change
-  	remove_column :markers, :latlng
-  	add_column :markers, :lat, :decimal
-  	add_column :markers, :lng, :decimal
-  end
-end
